7916928a3488f6039237b11a4daf33f64b7f3df0
Leonardo Cunha <agile.ice4816@fastmail.com>
2024-10-18 11:50:18 -0300

    remove unused style

--- src/static/Main.ts
+++ src/static/Main.ts
@@ -28,88 +28,90 @@ const Main = cmp(function Main(props: any) {
 
 <h1> ${model.Name} SDK Documentation</h1>
 
-<div class="container mx-auto flex flex-col md:flex-row mt-4">
-  <aside class="w-full md:w-1/4 bg-green-100 p-4">
+<div>
+  <aside>
       <nav>
           <ul>
-              <li class="mb-2"><a href="#introduction" class="text-green-700">Introduction</a></li>
-              <li class="mb-2"><a href="#getting-started" class="text-green-700">Getting Started</a></li>
-              <li class="mb-2"><a href="#examples" class="text-green-700">Examples</a></li>
+              <li><a href="#introduction">Introduction</a></li>
+              <li><a href="#getting-started">Getting Started</a></li>
+              <li><a href="#examples">Examples</a></li>
           </ul>
       </nav>
   </aside>
-<main class="w-full md:w-3/4 bg-white p-4">
-      <section id="introduction" class="mb-8">
-          <h2 class="text-2xl font-bold mb-4">Introduction</h2>
-          <p>Welcome to the ${model.Name} SDK documentation. This guide will help you integrate and use our SDK effectively.</p>
-      </section>
-      <section id="getting-started" class="mb-8">
-          <h2 class="text-2xl font-bold mb-4">Getting Started</h2>
-          <h3>Install SDK</h3>
-          <h4>JavaScript</h4>
-                    <pre><code class="language-javascript">
-npm install ${model.name}-sdk
-                        </code></pre>
-          <h3>Initialize SDK</h3>
-          <h4>JavaScript</h4>
-                    <pre><code class="language-javascript">
-const client = ${model.Name}SDK.make({
-  endpoint: process.env.${model.NAME}_ENDPOINT,
-  apikey: process.env.${model.NAME}_APIKEY,
-})
-                        </code></pre>
-      </section>
-`)
 
-  each(entity, (entity: any) => {
+  <main>
+    <section id="introduction">
+      <h2>Introduction</h2>
+      <p>Welcome to the ${model.Name} SDK documentation. This guide will help you integrate and use our SDK effectively.</p>
+    </section>
+    <section id="getting-started">
+      <h2>Getting Started</h2>
+
+      <h3>Install SDK</h3>
+      <h4>JavaScript</h4>
+      <pre><code>
+        npm install ${model.name}-sdk
+      </code></pre>
+
+      <h3>Initialize SDK</h3>
+      <h4>JavaScript</h4>
+        <pre><code>
+          const client = ${model.Name}SDK.make({
+            endpoint: process.env.${model.NAME}_ENDPOINT,
+            apikey: process.env.${model.NAME}_APIKEY,
+          })
+        </code></pre>
+      </section>
+    `)
+
+      each(entity, (entity: any) => {
+        Content(`
+    <section>
+      <h2>Create ${entity.Name}</h2>
+      <h4>JavaScript</h4>
+      <pre><code>
+        ${entity.name} = await client.${entity.Name}().create({
+          baa: "foo",
+        })
+      </code></pre>
+
+      <h2>Load ${entity.Name}</h2>
+      <h4>JavaScript</h4>
+      <pre><code>
+        ${entity.name} = await client.${entity.Name}().load({
+          id: 1
+        })
+      </code></pre>
+
+      <h2>List ${entity.Name}</h2>
+      <h4>JavaScript</h4>
+      <pre><code>
+        ${entity.name} = await client.${entity.Name}().List()
+      </code></pre>
+
+      <h2>Update ${entity.Name}</h2>
+      <h4>JavaScript</h4>
+      <pre><code>
+        ${entity.name} = await client.${entity.Name}().save({
+          baa: "foo",
+        })
+      </code></pre>
+
+      <h2>Remove ${entity.Name}</h2>
+      <h4>JavaScript</h4>
+      <pre><code>
+        ${entity.name} = await client.${entity.Name}().remove({
+          id: 1
+        })
+      </code></pre>
+    </section>
+  `)
+    })
     Content(`
-<section class="mb-8">
-  <h2 class="text-2xl font-bold mb-4">Create ${entity.Name}</h2>
-          <h4>JavaScript</h4>
-    <pre><code class="language-javascript">
-      ${entity.name} = await client.${entity.Name}().create({
-        baa: "foo",
-      })
-  </code></pre>
-
-  <h2 class="text-2xl font-bold mb-4">Load ${entity.Name}</h2>
-          <h4>JavaScript</h4>
-    <pre><code class="language-javascript">
-      ${entity.name} = await client.${entity.Name}().load({
-        id: 1
-      })
-  </code></pre>
-
-  <h2 class="text-2xl font-bold mb-4">List ${entity.Name}</h2>
-          <h4>JavaScript</h4>
-    <pre><code class="language-javascript">
-      ${entity.name} = await client.${entity.Name}().List()
-  </code></pre>
-
-  <h2 class="text-2xl font-bold mb-4">Update ${entity.Name}</h2>
-          <h4>JavaScript</h4>
-    <pre><code class="language-javascript">
-      ${entity.name} = await client.${entity.Name}().save({
-        baa: "foo",
-      })
-  </code></pre>
-
-  <h2 class="text-2xl font-bold mb-4">Remove ${entity.Name}</h2>
-          <h4>JavaScript</h4>
-    <pre><code class="language-javascript">
-      ${entity.name} = await client.${entity.Name}().remove({
-        id: 1
-      })
-  </code></pre>
-</section>
-`)
-  })
-  Content(`
-      <section id="examples" class="mb-8">
-          <h2 class="text-2xl font-bold mb-4">Examples</h2>
-          <p>Here are some examples on how to use our SDK:</p>
-          <!-- Example code snippets go here -->
-      </section>
+    <section id="examples">
+        <h2>Examples</h2>
+        <p>Here are some examples on how to use our SDK:</p>
+    </section>
   </main>
 </div>
         `)
