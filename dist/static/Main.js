2c7ffdeb6cdd92974a5dfa8dc92da1c44a6a535d
Leonardo Cunha <agile.ice4816@fastmail.com>
2024-10-18 09:39:46 -0300

    [wip] add markup

--- dist/static/Main.js
+++ dist/static/Main.js
@@ -9,6 +9,17 @@ const Main = (0, jostraca_1.cmp)(function Main(props) {
     (0, jostraca_1.Content)(`
 <style>
     @import url('https://cdn.tailwindcss.com');
+        /* Additional CSS for syntax highlighting */
+        pre {
+            background-color: #f9f9f9;
+            padding: 1rem;
+            border-radius: 0.5rem;
+            overflow-x: auto;
+        }
+        code {
+            color: #333;
+            font-family: Menlo, Monaco, 'Courier New', monospace;
+        }
 </style>
 
 <h1> ${model.Name} SDK Documentation</h1>
@@ -28,24 +39,67 @@ const Main = (0, jostraca_1.cmp)(function Main(props) {
           <h2 class="text-2xl font-bold mb-4">Introduction</h2>
           <p>Welcome to the ${model.Name} SDK documentation. This guide will help you integrate and use our SDK effectively.</p>
       </section>
+      <section id="getting-started" class="mb-8">
+          <h2 class="text-2xl font-bold mb-4">Getting Started</h2>
+          <h3>Install SDK</h3>
+          <h4>JavaScript</h4>
+                    <pre><code class="language-javascript">
+npm install ${model.name}-sdk
+                        </code></pre>
+          <h3>Initialize SDK</h3>
+          <h4>JavaScript</h4>
+                    <pre><code class="language-javascript">
+const client = ${model.Name}SDK.make({
+  endpoint: process.env.${model.NAME}_ENDPOINT,
+  apikey: process.env.${model.NAME}_APIKEY,
+})
+                        </code></pre>
+      </section>
 `);
     (0, jostraca_1.each)(entity, (entity) => {
         (0, jostraca_1.Content)(`
 <section class="mb-8">
-  <h2 class="text-2xl font-bold mb-4">${entity.Name}</h2>
+  <h2 class="text-2xl font-bold mb-4">Create ${entity.Name}</h2>
+          <h4>JavaScript</h4>
+    <pre><code class="language-javascript">
+      ${entity.name} = await client.${entity.Name}().create({
+        baa: "foo",
+      })
+  </code></pre>
+
+  <h2 class="text-2xl font-bold mb-4">Load ${entity.Name}</h2>
+          <h4>JavaScript</h4>
+    <pre><code class="language-javascript">
+      ${entity.name} = await client.${entity.Name}().load({
+        id: 1
+      })
+  </code></pre>
+
+  <h2 class="text-2xl font-bold mb-4">List ${entity.Name}</h2>
+          <h4>JavaScript</h4>
+    <pre><code class="language-javascript">
+      ${entity.name} = await client.${entity.Name}().List()
+  </code></pre>
+
+  <h2 class="text-2xl font-bold mb-4">Update ${entity.Name}</h2>
+          <h4>JavaScript</h4>
+    <pre><code class="language-javascript">
+      ${entity.name} = await client.${entity.Name}().save({
+        baa: "foo",
+      })
+  </code></pre>
+
+  <h2 class="text-2xl font-bold mb-4">Remove ${entity.Name}</h2>
+          <h4>JavaScript</h4>
+    <pre><code class="language-javascript">
+      ${entity.name} = await client.${entity.Name}().remove({
+        id: 1
+      })
+  </code></pre>
 </section>
 `);
     });
     (0, jostraca_1.Content)(`
-      <section id="getting-started" class="mb-8">
-          <h2 class="text-2xl font-bold mb-4">Getting Started</h2>
-          <p>To get started, follow these steps:</p>
-          <ol class="list-decimal ml-8 mt-4">
-              <li>Install the SDK</li>
-              <li>Initialize the SDK</li>
-              <li>Start using the SDK</li>
-          </ol>
-      </section>
       <section id="examples" class="mb-8">
           <h2 class="text-2xl font-bold mb-4">Examples</h2>
           <p>Here are some examples on how to use our SDK:</p>
