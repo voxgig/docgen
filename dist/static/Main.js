2e033b9e917a367c8b0163c7e8c72cb69ef7c56b
Leonardo Cunha <agile.ice4816@fastmail.com>
2024-10-22 06:18:08 -0300

    add go examples

--- dist/static/Main.js
+++ dist/static/Main.js
@@ -5,7 +5,7 @@ const jostraca_1 = require("jostraca");
 const Main = (0, jostraca_1.cmp)(function Main(props) {
     const { ctx$ } = props;
     const { model } = ctx$;
-    const { entity } = model.main.sdk;
+    const { entity, option } = model.main.sdk;
     (0, jostraca_1.Content)(`
 <style>
  @import url('index.css');
@@ -15,9 +15,7 @@ a {
 
 pre {
   background-color: var(--c1);
-  padding: var(--s1);
   border-radius: var(--s1);
-  overflow-x: auto;
 }
 
 code {
@@ -38,19 +36,49 @@ code {
       <h2>Getting Started</h2>
 
       <h3>Install SDK</h3>
-      <h4>JavaScript</h4>
-      <pre><code>
-        npm install ${model.name}-sdk
-      </code></pre>
+      <div>
+        <h4>JavaScript</h4>
+        <pre><code>
+          npm install ${model.name}-sdk
+        </code></pre>
+      </div>
+      <div>
+        <h4>Go</h4>
+        <pre><code>
+          go get ${model.name}
+        </code></pre>
+      </div>
 
       <h3>Initialize SDK</h3>
-      <h4>JavaScript</h4>
-        <pre><code>
-          const client = ${model.Name}SDK.make({
-            endpoint: process.env.${model.NAME}_ENDPOINT,
-            apikey: process.env.${model.NAME}_APIKEY,
-          })
-        </code></pre>
+      <div>
+        <h4>JavaScript</h4>
+          <pre><code>
+            const client = ${model.Name}SDK.make({
+              `);
+    (0, jostraca_1.each)(option, (opt) => {
+        if (opt.kind == "String") {
+            (0, jostraca_1.Content)(`${opt.name}: process.env.${model.NAME}_${opt.name.toUpperCase()},
+              `);
+        }
+    });
+    (0, jostraca_1.Content)(`})
+          </code></pre>
+        </div>
+      <div>
+        <h4>Go</h4>
+          <pre><code>
+            options := ${model.name}sdk.Options{`);
+    (0, jostraca_1.each)(option, (opt) => {
+        if (opt.kind == "String") {
+            const capName = opt.name.charAt(0).toUpperCase() + opt.name.substring(1, opt.name.length);
+            (0, jostraca_1.Content)(`
+            ${capName}: os.Getenv("${model.NAME}_${opt.name.toUpperCase()}"),`);
+        }
+    });
+    (0, jostraca_1.Content)(`
+            }
+          </code></pre>
+        </div>
       </section>
     `);
     (0, jostraca_1.each)(entity, (entity) => {
@@ -59,47 +87,121 @@ code {
                 (0, jostraca_1.Content)(`
     <section>
       <h2>${op.Name} ${entity.Name}</h2>
-      <h4>JavaScript</h4>
-      <pre><code>
-        ${entity.name} = await client.${entity.Name}().${op.name}()
-      </code></pre>
+      <div>
+        <h4>JavaScript</h4>
+        <pre><code>
+          ${entity.name} = await client.${entity.Name}().${op.name}()
+          console.log('${entity.Name}', ${entity.name})
+        </code></pre>
+      </div>
+      <div>
+        <h4>Go</h4>
+        <pre><code>
+          ${entity.name}, err := client.${entity.Name}().${op.Name}()
+          if err != nil {
+            log.Println("Error running ${entity.name} ${op.Name}:", err)
+            return
+          }
+
+          log.Printf("${entity.Name} %+v\\n", ${entity.name})
+        </code></pre>
+      </div>
                `);
             }
             else if (op.name == "create") {
                 (0, jostraca_1.Content)(`
     <section>
       <h2>${op.Name} ${entity.Name}</h2>
-      <h4>JavaScript</h4>
-      <pre><code>
-        ${entity.name} = await client.${entity.Name}().${op.name}({
-            baa: "foo",
-        })
-      </code></pre>
+      <div>
+        <h4>JavaScript</h4>
+        <pre><code>
+          ${entity.name} = await client.${entity.Name}().${op.name}({
+              baa: "foo",
+          })
+
+          console.log('${entity.Name}', ${entity.name})
+        </code></pre>
+      </div>
+      <div>
+        <h4>Go</h4>
+        <pre><code>
+          data := ${entity.Name}Data{
+            Foo: zed
+          }
+
+          ${entity.name}, err := client.${entity.Name}().${op.Name}(data)
+          if err != nil {
+            fmt.Println("Error running ${entity.Name} ${op.Name}:", err)
+            return
+          }
+
+          fmt.Printf("${entity.Name} %+v\\n", ${entity.name})
+        </code></pre>
+      </div>
                `);
             }
             else if (op.name == "save") {
                 (0, jostraca_1.Content)(`
     <section>
       <h2>${op.Name} ${entity.Name}</h2>
-      <h4>JavaScript</h4>
-      <pre><code>
-        ${entity.name} = await client.${entity.Name}().${op.name}({
-            id: 1,
-            baa: "foo",
-        })
-      </code></pre>
+      <div>
+        <h4>JavaScript</h4>
+        <pre><code>
+          ${entity.name} = await client.${entity.Name}().${op.name}({
+              id: 1,
+              baa: "foo",
+          })
+
+          console.log('${entity.Name}', ${entity.name})
+        </code></pre>
+      </div>
+      <div>
+        <h4>Go</h4>
+        <pre><code>
+          data := ${entity.Name}Data{
+            Id: 1
+            Foo: zed
+          }
+
+          ${entity.name}, err := client.${entity.Name}().${op.Name}(data)
+          if err != nil {
+            fmt.Println("Error running ${entity.Name} ${op.Name}:", err)
+            return
+          }
+
+          fmt.Printf("${entity.Name} %+v\\n", ${entity.name})
+        </code></pre>
+      </div>
                `);
             }
             else {
                 (0, jostraca_1.Content)(`
     <section>
       <h2>${op.Name} ${entity.Name}</h2>
-      <h4>JavaScript</h4>
-      <pre><code>
-        ${entity.name} = await client.${entity.Name}().${op.name}({
-            id: 1
-        })
-      </code></pre>
+      <div>
+        <h4>JavaScript</h4>
+        <pre><code>
+          ${entity.name} = await client.${entity.Name}().${op.name}({
+              id: 1
+          })
+        </code></pre>
+      </div>
+      <div>
+        <h4>Go</h4>
+        <pre><code>
+          query := Query{
+            Id: 1
+          }
+
+          ${entity.name}, err := client.${entity.Name}().${op.Name}(query)
+          if err != nil {
+            fmt.Println("Error running ${entity.Name} ${op.Name}:", err)
+            return
+          }
+
+          fmt.Printf("${entity.Name} %+v\\n", ${entity.name})
+        </code></pre>
+      </div>
                `);
             }
         });
--- src/static/Main.ts
+++ src/static/Main.ts
@@ -8,7 +8,7 @@ const Main = cmp(function Main(props: any) {
   const { ctx$ } = props
   const { model } = ctx$
 
-  const { entity } = model.main.sdk
+  const { entity, option } = model.main.sdk
 
   Content(`
 <style>
@@ -19,9 +19,7 @@ a {
 
 pre {
   background-color: var(--c1);
-  padding: var(--s1);
   border-radius: var(--s1);
-  overflow-x: auto;
 }
 
 code {
@@ -42,19 +40,49 @@ code {
       <h2>Getting Started</h2>
 
       <h3>Install SDK</h3>
-      <h4>JavaScript</h4>
-      <pre><code>
-        npm install ${model.name}-sdk
-      </code></pre>
+      <div>
+        <h4>JavaScript</h4>
+        <pre><code>
+          npm install ${model.name}-sdk
+        </code></pre>
+      </div>
+      <div>
+        <h4>Go</h4>
+        <pre><code>
+          go get ${model.name}
+        </code></pre>
+      </div>
 
       <h3>Initialize SDK</h3>
-      <h4>JavaScript</h4>
-        <pre><code>
-          const client = ${model.Name}SDK.make({
-            endpoint: process.env.${model.NAME}_ENDPOINT,
-            apikey: process.env.${model.NAME}_APIKEY,
-          })
-        </code></pre>
+      <div>
+        <h4>JavaScript</h4>
+          <pre><code>
+            const client = ${model.Name}SDK.make({
+              `)
+  each(option, (opt: any) => {
+    if (opt.kind == "String") {
+      Content(`${opt.name}: process.env.${model.NAME}_${opt.name.toUpperCase()},
+              `)
+    }
+  })
+  Content(`})
+          </code></pre>
+        </div>
+      <div>
+        <h4>Go</h4>
+          <pre><code>
+            options := ${model.name}sdk.Options{`)
+  each(option, (opt: any) => {
+    if (opt.kind == "String") {
+      const capName = opt.name.charAt(0).toUpperCase() + opt.name.substring(1, opt.name.length);
+      Content(`
+            ${capName}: os.Getenv("${model.NAME}_${opt.name.toUpperCase()}"),`)
+    }
+  })
+  Content(`
+            }
+          </code></pre>
+        </div>
       </section>
     `)
 
@@ -64,44 +92,118 @@ code {
         Content(`
     <section>
       <h2>${op.Name} ${entity.Name}</h2>
-      <h4>JavaScript</h4>
-      <pre><code>
-        ${entity.name} = await client.${entity.Name}().${op.name}()
-      </code></pre>
+      <div>
+        <h4>JavaScript</h4>
+        <pre><code>
+          ${entity.name} = await client.${entity.Name}().${op.name}()
+          console.log('${entity.Name}', ${entity.name})
+        </code></pre>
+      </div>
+      <div>
+        <h4>Go</h4>
+        <pre><code>
+          ${entity.name}, err := client.${entity.Name}().${op.Name}()
+          if err != nil {
+            log.Println("Error running ${entity.name} ${op.Name}:", err)
+            return
+          }
+
+          log.Printf("${entity.Name} %+v\\n", ${entity.name})
+        </code></pre>
+      </div>
                `)
       } else if (op.name == "create") {
         Content(`
     <section>
       <h2>${op.Name} ${entity.Name}</h2>
-      <h4>JavaScript</h4>
-      <pre><code>
-        ${entity.name} = await client.${entity.Name}().${op.name}({
-            baa: "foo",
-        })
-      </code></pre>
+      <div>
+        <h4>JavaScript</h4>
+        <pre><code>
+          ${entity.name} = await client.${entity.Name}().${op.name}({
+              baa: "foo",
+          })
+
+          console.log('${entity.Name}', ${entity.name})
+        </code></pre>
+      </div>
+      <div>
+        <h4>Go</h4>
+        <pre><code>
+          data := ${entity.Name}Data{
+            Foo: zed
+          }
+
+          ${entity.name}, err := client.${entity.Name}().${op.Name}(data)
+          if err != nil {
+            fmt.Println("Error running ${entity.Name} ${op.Name}:", err)
+            return
+          }
+
+          fmt.Printf("${entity.Name} %+v\\n", ${entity.name})
+        </code></pre>
+      </div>
                `)
       } else if (op.name == "save") {
         Content(`
     <section>
       <h2>${op.Name} ${entity.Name}</h2>
-      <h4>JavaScript</h4>
-      <pre><code>
-        ${entity.name} = await client.${entity.Name}().${op.name}({
-            id: 1,
-            baa: "foo",
-        })
-      </code></pre>
+      <div>
+        <h4>JavaScript</h4>
+        <pre><code>
+          ${entity.name} = await client.${entity.Name}().${op.name}({
+              id: 1,
+              baa: "foo",
+          })
+
+          console.log('${entity.Name}', ${entity.name})
+        </code></pre>
+      </div>
+      <div>
+        <h4>Go</h4>
+        <pre><code>
+          data := ${entity.Name}Data{
+            Id: 1
+            Foo: zed
+          }
+
+          ${entity.name}, err := client.${entity.Name}().${op.Name}(data)
+          if err != nil {
+            fmt.Println("Error running ${entity.Name} ${op.Name}:", err)
+            return
+          }
+
+          fmt.Printf("${entity.Name} %+v\\n", ${entity.name})
+        </code></pre>
+      </div>
                `)
       } else {
         Content(`
     <section>
       <h2>${op.Name} ${entity.Name}</h2>
-      <h4>JavaScript</h4>
-      <pre><code>
-        ${entity.name} = await client.${entity.Name}().${op.name}({
-            id: 1
-        })
-      </code></pre>
+      <div>
+        <h4>JavaScript</h4>
+        <pre><code>
+          ${entity.name} = await client.${entity.Name}().${op.name}({
+              id: 1
+          })
+        </code></pre>
+      </div>
+      <div>
+        <h4>Go</h4>
+        <pre><code>
+          query := Query{
+            Id: 1
+          }
+
+          ${entity.name}, err := client.${entity.Name}().${op.Name}(query)
+          if err != nil {
+            fmt.Println("Error running ${entity.Name} ${op.Name}:", err)
+            return
+          }
+
+          fmt.Printf("${entity.Name} %+v\\n", ${entity.name})
+        </code></pre>
+      </div>
                `)
       }
     })
